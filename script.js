b3f6dc1c60d5aa46e971e404d6110583b6b26c7a
Wahyudi28 <pccadangan@gmail.com>
2026-04-25 13:31:54 +0800

    first commit

--- script.js
+++ script.js
@@ -127,7 +127,8 @@ function getGreeting(hour) {
 function formatTime(date) {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  return `${hours}:${minutes}`;
+  const second = String(date.getSecond()).padStart(2, "0");
+  return `${hours}:${minutes}:${second}`;
 }
 
 /**
